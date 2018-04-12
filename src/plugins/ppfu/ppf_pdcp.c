54a7c29c12646ef81539220956c9543470073429
Jordy You <jordy.you@ericsson.com>
2018-04-12 07:38:34 -0400

    add pdcp cli/api

--- src/plugins/ppfu/ppf_pdcp.c
+++ src/plugins/ppfu/ppf_pdcp.c
@@ -67,34 +67,6 @@ ppf_pdcp_init (vlib_main_t * vm)
 VLIB_INIT_FUNCTION (ppf_pdcp_init);
 
 
-u8 *
-format_ppf_pdcp_session (u8 * s, va_list * va)
-{
-  ppf_pdcp_session_t * pdcp_session = va_arg (*va, ppf_pdcp_session_t *);
-
-  s = format (s, "sn-length %d, header-length %d, mac-length %d, in-flight-limit %lx\n", 
-  	pdcp_session->sn_length, 
-  	pdcp_session->header_length,
-  	pdcp_session->mac_length,
-  	pdcp_session->in_flight_limit);
-
-  s = format (s, "tx-hfn %u, tx-next %u\n", 
-  	pdcp_session->tx_hfn, 
-  	pdcp_session->tx_next_sn);
-
-  s = format (s, "rx-hfn %u, rx-next-expected %u, rx-last-forwarded %u\n", 
-  	pdcp_session->rx_hfn, 
-  	pdcp_session->rx_next_expected_sn, 
-  	pdcp_session->rx_last_forwarded_sn);
-
-  s = format (s, "security-alg %x, intergity-key %U, crypto-key %U\n", 
-  	pdcp_session->security_algorithms, 
-  	format_hex_bytes, pdcp_session->integrity_key, MAX_PDCP_KEY_LEN,
-  	format_hex_bytes, pdcp_session->crypto_key, MAX_PDCP_KEY_LEN);
-
-  return s;
-}
-
 void 
 ppf_pdcp_encap_header_5bsn (u8 * buf, u8 dc, u32 sn)
 {
@@ -237,14 +209,7 @@ ppf_pdcp_nop (u8 * in, u8 * out, u32 size, void * security_parameters)
 }
 
 u32
-ppf_pdcp_eia0_protect (u8 * in, u8 * out, u32 size, void * security_parameters)
-{
-	memset (out, 0x00, 4);
-	return 0;
-}
-
-u32
-ppf_pdcp_eia0_validate (u8 * in, u8 * out, u32 size, void * security_parameters)
+ppf_pdcp_eia0 (u8 * in, u8 * out, u32 size, void * security_parameters)
 {
 	memset (out, 0x00, 4);
 	return 0;
@@ -378,11 +343,12 @@ ppf_pdcp_create_session (u8 sn_length, u32 rx_count, u32 tx_count, u32 in_flight
   vec_validate_init_empty (pdcp_sess->rx_reorder_buffers, MAX_REORDER_WINDOW_SIZE, INVALID_BUFFER_INDEX);
 
   pdcp_sess->sn_length = sn_length;
-  pdcp_sess->tx_next_sn = tx_count; //(~(0xFFFFFFFF<<sn_length))&tx_count;
-  pdcp_sess->tx_hfn = tx_count >> sn_length;
-  pdcp_sess->rx_next_expected_sn = rx_count; //(~(0xFFFFFFFF<<sn_length))&rx_count;
-  pdcp_sess->rx_last_forwarded_sn = (((pdcp_sess->rx_next_expected_sn - 1) & ((1 << sn_length) - 1)) & max_sn);
-  pdcp_sess->rx_hfn = ((rx_count >> sn_length) - 1) & (max_sn >> sn_length);
+  pdcp_sess->tx_next_sn = PPF_PDCP_SN (tx_count, sn_length);
+  pdcp_sess->tx_hfn = PPF_PDCP_HFN (tx_count, sn_length);
+  pdcp_sess->rx_next_expected_sn = PPF_PDCP_SN (rx_count, sn_length);
+  pdcp_sess->rx_last_forwarded_sn = PPF_PDCP_SN_DEC (pdcp_sess->rx_next_expected_sn, sn_length - 1) & max_sn;
+  pdcp_sess->rx_hfn = PPF_PDCP_HFN (rx_count, sn_length);
+  // why? pdcp_sess->rx_hfn = PPF_PDCP_COUNT_HFN_DEC (rx_count, sn_length);
 
   if (in_flight_limit == max_sn) // no value configured by upper layers
     pdcp_sess->in_flight_limit = 0x1 << (sn_length - 1); //use default value: SN-range/2 - 1
@@ -460,8 +426,8 @@ ppf_pdcp_session_update_as_security (ppf_pdcp_session_t * pdcp_sess, ppf_pdcp_co
 	
       case PDCP_EIA0:
         pdcp_sess->security_algorithms |= PDCP_NULL_INTEGRITY;
-      	pdcp_sess->protect = &ppf_pdcp_eia0_protect;
-        pdcp_sess->validate = &ppf_pdcp_eia0_validate;
+      	pdcp_sess->protect = &ppf_pdcp_eia0;
+        pdcp_sess->validate = &ppf_pdcp_eia0;
         pdcp_sess->mac_length = 4;
         break;
 	
@@ -555,6 +521,273 @@ ppf_pdcp_clear_session (ppf_pdcp_session_t * pdcp_sess)
 
   return 0;
 }
+
+#define foreach_pdcp_crypto_alg   \
+  _(0, EEA0, "none")              \
+  _(1, EEA1, "snow-3g")           \
+  _(2, EEA2, "aes-ctr-128")       \
+  _(3, EEA3, "zuc")
+
+#define foreach_pdcp_integ_alg    \
+  _(0, EIA_NONE, "none") 	      \
+  _(1, EIA0, "null")              \
+  _(2, EIA1, "snow-3g")           \
+  _(3, EIA2, "aes-cmac-128")      \
+  _(4, EIA3, "zuc")
+
+#define foreach_pdcp_security_alg        \
+  _(0, NONE_SECURITY, "non-configured")  \
+  _(0x01, NULL_CIPHERING, "null")        \
+  _(0x02, SNOW3G_CIPHERING, "snow-3g")   \
+  _(0x04, AES_CIPHERING, "aes-ctr-128")  \
+  _(0x08, ZUC_CIPHERING, "zuc")          \
+  _(0x10, NULL_INTEGRITY, "null")		 \
+  _(0x20, SNOW3G_INTEGRITY, "snow-3g") 	 \
+  _(0x40, AES_INTEGRITY, "aes-cmac-128") \
+  _(0x80, ZUC_INTEGRITY, "zuc")
+
+
+u8 *
+format_pdcp_crypto_alg (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  u8 *t = 0;
+
+  switch (i)
+    {
+#define _(v,f,str) case PDCP_##f: t = (u8 *) str; break;
+      foreach_pdcp_crypto_alg
+#undef _
+    default:
+      s = format (s, "unknown");
+    }
+  s = format (s, "%s", t);
+  return s;
+}
+
+uword
+unformat_pdcp_crypto_alg (unformat_input_t * input, va_list * args)
+{
+  u32 *r = va_arg (*args, u32 *);
+
+  if (0);
+#define _(v,f,s) else if (unformat (input, s)) *r = PDCP_##f;
+  foreach_pdcp_crypto_alg
+#undef _
+    else
+    return 0;
+  return 1;
+}
+
+u8 *
+format_pdcp_integ_alg (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  u8 *t = 0;
+
+  switch (i)
+    {
+#define _(v,f,str) case PDCP_##f: t = (u8 *) str; break;
+      foreach_pdcp_integ_alg
+#undef _
+    default:
+      s = format (s, "unknown");
+    }
+  s = format (s, "%s", t);
+  return s;
+}
+
+uword
+unformat_pdcp_integ_alg (unformat_input_t * input, va_list * args)
+{
+  u32 *r = va_arg (*args, u32 *);
+
+  if (0);
+#define _(v,f,s) else if (unformat (input, s)) *r = PDCP_##f;
+  foreach_pdcp_integ_alg
+#undef _
+    else
+    return 0;
+  return 1;
+}
+
+u8 *
+format_pdcp_security_alg (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  u8 *t = 0;
+
+  switch (i)
+    {
+#define _(v,f,str) case PDCP_##f: t = (u8 *) str; break;
+      foreach_pdcp_security_alg
+#undef _
+    default:
+      s = format (s, "unknown");
+    }
+  s = format (s, "%s", t);
+  return s;
+}
+
+u8 *
+format_ppf_pdcp_session (u8 * s, va_list * va)
+{
+  ppf_pdcp_session_t * pdcp_session = va_arg (*va, ppf_pdcp_session_t *);
+
+  s = format (s, "sn-length %d, header-length %d, mac-length %d, in-flight-limit %lx\n", 
+  	pdcp_session->sn_length, 
+  	pdcp_session->header_length,
+  	pdcp_session->mac_length,
+  	pdcp_session->in_flight_limit);
+
+  s = format (s, "tx-hfn %u, tx-next %u\n", 
+  	pdcp_session->tx_hfn, 
+  	pdcp_session->tx_next_sn);
+
+  s = format (s, "rx-hfn %u, rx-next-expected %u, rx-last-forwarded %u\n", 
+  	pdcp_session->rx_hfn, 
+  	pdcp_session->rx_next_expected_sn, 
+  	pdcp_session->rx_last_forwarded_sn);
+
+  s = format (s, "integrity-alg %U, intergity-key %U\n", 
+  	format_pdcp_security_alg, pdcp_session->security_algorithms & 0xf0, 
+  	format_hex_bytes, pdcp_session->integrity_key, MAX_PDCP_KEY_LEN);
+
+  s = format (s, "crypto-alg %U, crypto-key %U\n", 
+  	format_pdcp_security_alg, pdcp_session->security_algorithms & 0x0f, 
+  	format_hex_bytes, pdcp_session->crypto_key, MAX_PDCP_KEY_LEN);
+
+  return s;
+}
+
+
+static clib_error_t *
+ppf_pdcp_add_del_session_command_fn (vlib_main_t * vm,
+										unformat_input_t * input,
+										vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u8 is_add = 1;
+  u32 call_id = ~0;
+  u32 sn_length = ~0, init_ul_count = 1, init_dl_count = 1, in_flight_limit = 0;
+  u32 alg;
+  u8 *key = 0;
+  ppf_pdcp_config_t pdcp_cfg;
+  clib_error_t *error = NULL;
+  ppf_callline_t * callline;
+  u32 sess_id = ~0;
+
+  memset (&pdcp_cfg, 0, sizeof (pdcp_cfg));
+  
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "del"))
+      {
+        is_add = 0;
+      }
+      else if (unformat (line_input, "call-id %d", &call_id))
+      ;
+      else if (unformat (line_input, "sn-length %d", &sn_length))
+      ;
+      else if (unformat (line_input, "init-ul-count %d", &init_ul_count))
+      ;
+      else if (unformat (line_input, "init-dl-count %d", &init_dl_count))
+      ;
+      else if (unformat (line_input, "in-flight-limit %d", &in_flight_limit))
+      ;
+      else if (unformat (line_input, "crypto-alg %U", unformat_pdcp_crypto_alg, &alg)) {
+        pdcp_cfg.flags |= CRYPTO_ALG_VALID;
+		pdcp_cfg.crypto_algorithm = alg;
+      }
+      else if (unformat (line_input, "integ-alg %U", unformat_pdcp_integ_alg, &alg)) {
+	    pdcp_cfg.flags |= INTEGRITY_ALG_VALID;
+		pdcp_cfg.integrity_algorithm = alg;
+      }
+      else if (unformat (line_input, "CK %U", unformat_hex_string, &key)) {
+	  	if (vec_len (key) >= MAX_PDCP_KEY_LEN) {
+          pdcp_cfg.flags |= CRYPTO_KEY_VALID;
+          clib_memcpy (pdcp_cfg.crypto_key, key, MAX_PDCP_KEY_LEN);
+	  	}
+      }
+      else if (unformat (line_input, "IK %U", unformat_hex_string, &key)) {
+	  	if (vec_len (key) >= MAX_PDCP_KEY_LEN) {
+          pdcp_cfg.flags |= INTEGRITY_KEY_VALID;
+          clib_memcpy (pdcp_cfg.integrity_key, key, MAX_PDCP_KEY_LEN);
+	  	}
+      }
+      else
+      {
+        error = clib_error_return (0, "parse error: '%U'",
+        format_unformat_error, line_input);
+        goto done;
+      }
+    }
+    
+  if (call_id == ~0) 
+   {
+      error = clib_error_return (0, "call-id not specified");
+      goto done;
+   }
+
+  if (is_add == 1 && sn_length == ~0) 
+   {
+      error = clib_error_return (0, "sn-length not specified");
+      goto done;
+   }
+
+  if (call_id >= ppf_main.max_capacity)
+  {
+	error = clib_error_return (0, "error call-id %u, should be in range[0, %u)", call_id, ppf_main.max_capacity);
+	goto done;
+  }
+  
+  callline = &(ppf_main.ppf_calline_table[call_id]);
+    
+  if (is_add == 1) {
+  	sess_id = ppf_pdcp_create_session (sn_length, init_ul_count, init_dl_count, in_flight_limit);
+	if (~0 != sess_id) {
+		ppf_pdcp_session_update_as_security (pool_elt_at_index(ppf_pdcp_main.sessions, sess_id), &pdcp_cfg);
+	} else {
+		error = clib_error_return (0, "create session fail, error sn-length %u", sn_length);
+		goto done;
+	}
+
+	callline->pdcp.session_id = sess_id;
+    vlib_cli_output (vm, "[PDCP session created], details: %U",
+		format_ppf_pdcp_session, pool_elt_at_index(ppf_pdcp_main.sessions, sess_id));
+  }
+  else { /* del */
+  	if (~0 != callline->pdcp.session_id) {
+      ppf_pdcp_clear_session (pool_elt_at_index (ppf_pdcp_main.sessions, callline->pdcp.session_id));
+      callline->pdcp.session_id = ~0;
+  	} else {
+	  error = clib_error_return (0, "delete session fail, error call-id %u", call_id);
+	  goto done;
+  	}
+  }
+
+done:
+  unformat_free (line_input);
+
+  return error;
+}
+
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (create_ppf_pdcp_session_command, static) = {
+  .path = "create ppf_pdcp session",
+  .short_help =	  
+  "create ppf_pdcp session call-id <nn> sn-length <nn> "
+  " [init-ul-count <nn>] [init-dl-count <nn>] [in-flight-limit <nn>] "  
+  " [crypto-alg <alg>|integ-alg <alg>] [CK <key>|IK <key>] [del]",
+  .function = ppf_pdcp_add_del_session_command_fn,
+};
+/* *INDENT-ON* */
+
 
 /*
  * fd.io coding-style-patch-verification: ON
