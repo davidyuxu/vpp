85d0e3e77dffb92ac149ffd2a7e17430fbe0cf4e
Jordy You <jordy.you@ericsson.com>
2018-05-21 07:56:27 -0400

    ipv6 for transport cleanup

--- src/vnet/ipsec/ipsec_if_in.c
+++ src/vnet/ipsec/ipsec_if_in.c
@@ -178,6 +178,7 @@ ipsec_if_input_node_fn (vlib_main_t * vm, vlib_node_runtime_t * node,
 		  vnet_buffer (b0)->ipsec.flags = IPSEC_FLAG_IPSEC_GRE_TUNNEL;
 		}
 
+	      vlib_buffer_advance (b0, ip4_header_bytes (ip0));
 	      next0 = im->esp_decrypt_next_index;
 	    }
 
